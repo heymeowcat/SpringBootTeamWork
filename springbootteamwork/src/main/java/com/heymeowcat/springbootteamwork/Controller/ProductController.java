c0fb4b8b6d578af05800b36ed5afc396df32c92e
Maduranjana Janakantha <43213319+maduranjana@users.noreply.github.com>
2021-03-31 19:24:38 +0530

    Product Controller Added 🚗

--- springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/Controller/ProductController.java
+++ springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/Controller/ProductController.java
@@ -1,5 +1,57 @@
 package com.heymeowcat.springbootteamwork.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.heymeowcat.springbootteamwork.Service.ProductService;
+import com.heymeowcat.springbootteamwork.models.Products;
+
+@RestController
+@RequestMapping(path = "/Products")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductController {
+
+	@Autowired
+	ProductService productService;
+
+	@GetMapping("/getAllProducts")
+	public List<Products> getAllProducts() {
+		return productService.getAllProducts();
+	}
+
+	@PostMapping("/addProducts")
+	public List<Products> addProducts(@RequestBody List<Products> pList) {
+		return productService.addProducts(pList);
+	}
+
+	@PostMapping("/addProduct")
+	public Products addProduct(@RequestBody Products product) {
+		return productService.addProduct(product);
+	}
+
+	@PutMapping("/updateProduct")
+	public Products updateProduct(@RequestBody Products product) {
+		return productService.updateProduct(product);
+	}
+
+	@DeleteMapping("{id}")
+	public String deleteProduct(@PathVariable int id) {
+		return productService.deleteProduct(id);
+	}
+
+	@GetMapping("{id}")
+	public Products getProduct(@PathVariable int id) {
+		return productService.getProductById(id);
+	}
 
 }
