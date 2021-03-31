e29a96966f146afe6f74c68a060ab67ec23fd9ae
Vidura Fernando <heyvidura@gmail.com>
2021-03-31 20:07:20 +0530

    Added @JsonIgnoreProperties to product list in Categories Entity

--- springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/models/Categories.java
+++ springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/models/Categories.java
@@ -13,8 +13,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "Categories") 
+@JsonIgnoreProperties({ "products" })
 public class Categories implements Serializable{
     private static final long serialVersionUID = 1L;
 
