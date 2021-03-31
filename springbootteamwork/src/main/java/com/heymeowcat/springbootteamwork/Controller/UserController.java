784cede8cac071452ba56051b3714cc862a29f8d
Vidura Fernando <heyvidura@gmail.com>
2021-03-31 17:51:03 +0530

    Update CrossOrigin to *

--- springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/Controller/UserController.java
+++ springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/Controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/Users")
-@CrossOrigin(origins = "http://localhost", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
     @Autowired
