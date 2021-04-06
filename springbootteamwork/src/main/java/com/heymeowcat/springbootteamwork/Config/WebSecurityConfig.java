9073e9432d0a555d4789eadb0afa73ca7063a6f0
Vidura Fernando <heyvidura@gmail.com>
2021-04-06 13:46:38 +0530

    Disabled Cors 😀

--- springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/Config/WebSecurityConfig.java
+++ springbootteamwork/src/main/java/com/heymeowcat/springbootteamwork/Config/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity.csrf().disable()
+		httpSecurity.cors().and().csrf().disable()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/Users/authenticate","/Users/addUser","/Users/saveUser").permitAll().
 				// all other requests need to be authenticated
